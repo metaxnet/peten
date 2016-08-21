d00b2a1a3001954fe1ea8fb6cce3a4cc101dd09b
metaxnet <metaxnet@gmail.com>
2016-08-21 07:13:21 +0200

    fixing?

--- temp.py
+++ temp.py
@@ -1,19 +1,2 @@
 # coding=UTF-8
-# כלב = _word1
-# שם = _word2
-# נבח = _word3
-# מוקי = _word4
-
-class _word1:
-        def __init__(self, _word2):
-                self._word2 = _word2
-
-        def _word3(self, _word2):
-                if self._word2 == _word2:
-                        print "הב הב!"
-                else:
-                        print "..."
-
-_word4 = _word1("מוקי")
-_word4._word3("מיצי")
-_word4._word3("מוקי")
+print("שלום")
